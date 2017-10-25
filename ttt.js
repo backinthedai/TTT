696f952c614f82ec1d270cb6959b1581656e8a08
Dai Phung <dai@daiphung.com>
2017-10-25 18:03:37 +1100

    Add calculation conditions

--- ttt.js
+++ ttt.js
@@ -10,12 +10,11 @@ let board = [
 
 let tmpBoard = board.slice(0); //make a copy for ai
 let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-
-const boardLen = 9;
 let winner = "";
 
+const boardLen = 9;
+
 function main(e) {
-    // console.log(e.target.id);
     markBoard(e.target.id);   
 }
 
@@ -67,10 +66,45 @@ const endGame=()=>{
 }
 
 const calculate = () =>{
-    if(board[0]['b0'] === board[1]['b1'] && board[1]['b1'] === board[2]['b2']){
+    if(board[0]['b0'] === board[1]['b1'] && board[1]['b1'] === board[2]['b2'] && board[1]['b1'] !== ""){
         setWinner(board[0]['b0']);       
         setWinnerColor([0, 1, 2]);
-
+        //row 1
+    }
+    if(board[3]['b3'] === board[4]['b4'] && board[4]['b4'] === board[5]['b5'] && board[4]['b4'] !== ""){
+        setWinner(board[3]['b3']);       
+        setWinnerColor([3, 4, 5]);
+        //row 2
+    }
+    if(board[6]['b6'] === board[7]['b7'] && board[7]['b7'] === board[8]['b8'] && board[7]['b7'] !== ""){
+        setWinner(board[6]['b6']);       
+        setWinnerColor([6, 7, 8]);
+        // row 3
+    }
+    if(board[0]['b0'] === board[3]['b3'] && board[3]['b3'] === board[6]['b6'] && board[3]['b3'] !== ""){
+        setWinner(board[0]['b0']);       
+        setWinnerColor([0, 3, 6]);
+        // column 1
+    }
+    if(board[1]['b1'] === board[4]['b4'] && board[4]['b4'] === board[7]['b7'] && board[4]['b4'] !== ""){
+        setWinner(board[1]['b1']);       
+        setWinnerColor([1, 4, 7]);
+        // column 2
+    }
+    if(board[2]['b2'] === board[5]['b5'] && board[5]['b5'] === board[8]['b8'] && board[5]['b5'] !== ""){
+        setWinner(board[2]['b2']);       
+        setWinnerColor([2, 5, 8]);
+        // column 3
+    }
+    if(board[0]['b0'] === board[4]['b4'] && board[4]['b4'] === board[8]['b8'] && board[4]['b4'] !== ""){
+        setWinner(board[0]['b0']);       
+        setWinnerColor([0, 4, 8]);
+        // diagonal from top left
+    }
+    if(board[2]['b2'] === board[4]['b4'] && board[4]['b4'] === board[6]['b6'] && board[4]['b4'] !== ""){
+        setWinner(board[2]['b2']);       
+        setWinnerColor([2, 4, 6]);
+        // diagonal from top right
     }
 }
 
@@ -79,7 +113,7 @@ const setWinner = (winSymbol)=>{
 }
 
 const setWinnerColor = (arr)=>{
-    for (var i = 0; i< arr.length; i++){
-        document.getElementById(`b${i}`).style.color = '#ff0000';
+    for (var i = 0; i < arr.length; i++){
+        document.getElementById(`b${arr[i]}`).style.color = '#ff0000';
     }
 }
