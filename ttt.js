e673cf505e196566514c6dfb17c00c9f5015016e
Dai Phung <dai@daiphung.com>
2017-10-26 14:46:25 +1100

    Add no win scenerio "Draw"

--- ttt.js
+++ ttt.js
@@ -20,20 +20,20 @@ function main(e) {
 
 const markBoard = (id) => {
 
-    let isWinner = human(id);
-
-    if (isWinner) {
+    if (human(id)) {
         console.log("Human is Winner");
         delay = setTimeout(restartGame, 2000);
     }
-    else {
-        isWinner = ai();
-
-        if(isWinner){
-            console.log("AI is Winner");
+    else if (ai()) {
+        console.log("AI is Winner");
+        delay = setTimeout(restartGame, 2000);
+    }
+    else{
+        if (arr.length === 0) {
+            console.log("Draw");
             delay = setTimeout(restartGame, 2000);
         }
-    }
+    }   
 }
 
 const human = (id) => {
@@ -73,7 +73,6 @@ const updateBoard = (id) => {
 
 const restartGame = () => {
     //end the game and restart the game
-    //update stats
     board = [
         { 'b0': "" }, { 'b1': "" }, { 'b2': "" },
         { 'b3': "" }, { 'b4': "" }, { 'b5': "" },
@@ -90,49 +89,39 @@ const restartGame = () => {
     clearTimeout(delay);
 }
 
-
 const calculate = () => {
-    if (board[0]['b0'] === board[1]['b1'] && board[1]['b1'] === board[2]['b2'] && board[1]['b1'] !== "") {     
+    if (board[0]['b0'] === board[1]['b1'] && board[1]['b1'] === board[2]['b2'] && board[1]['b1'] !== "") {
         setWinnerColor([0, 1, 2]);
         return true;
-        //row 1
     }
-    if (board[3]['b3'] === board[4]['b4'] && board[4]['b4'] === board[5]['b5'] && board[4]['b4'] !== "") {   
+    if (board[3]['b3'] === board[4]['b4'] && board[4]['b4'] === board[5]['b5'] && board[4]['b4'] !== "") {
         setWinnerColor([3, 4, 5]);
         return true;
-        //row 2
     }
     if (board[6]['b6'] === board[7]['b7'] && board[7]['b7'] === board[8]['b8'] && board[7]['b7'] !== "") {
         setWinnerColor([6, 7, 8]);
         return true;
-        // row 3
     }
-    if (board[0]['b0'] === board[3]['b3'] && board[3]['b3'] === board[6]['b6'] && board[3]['b3'] !== "") { 
+    if (board[0]['b0'] === board[3]['b3'] && board[3]['b3'] === board[6]['b6'] && board[3]['b3'] !== "") {
         setWinnerColor([0, 3, 6]);
         return true;
-        // column 1
     }
-    if (board[1]['b1'] === board[4]['b4'] && board[4]['b4'] === board[7]['b7'] && board[4]['b4'] !== "") {     
+    if (board[1]['b1'] === board[4]['b4'] && board[4]['b4'] === board[7]['b7'] && board[4]['b4'] !== "") {
         setWinnerColor([1, 4, 7]);
         return true;
-        // column 2
     }
-    if (board[2]['b2'] === board[5]['b5'] && board[5]['b5'] === board[8]['b8'] && board[5]['b5'] !== "") {    
+    if (board[2]['b2'] === board[5]['b5'] && board[5]['b5'] === board[8]['b8'] && board[5]['b5'] !== "") {
         setWinnerColor([2, 5, 8]);
         return true;
-        // column 3
     }
-    if (board[0]['b0'] === board[4]['b4'] && board[4]['b4'] === board[8]['b8'] && board[4]['b4'] !== "") {     
+    if (board[0]['b0'] === board[4]['b4'] && board[4]['b4'] === board[8]['b8'] && board[4]['b4'] !== "") {
         setWinnerColor([0, 4, 8]);
         return true;
-        // diagonal from top left
     }
-    if (board[2]['b2'] === board[4]['b4'] && board[4]['b4'] === board[6]['b6'] && board[4]['b4'] !== "") {   
+    if (board[2]['b2'] === board[4]['b4'] && board[4]['b4'] === board[6]['b6'] && board[4]['b4'] !== "") {
         setWinnerColor([2, 4, 6]);
         return true;
-        // diagonal from top right
     }
-    return false;
 }
 
 const setWinnerColor = (arr) => {
